50cd6aa8700551992258afd4bdcc507e741c6164
Kris Reeves <kris.re@bbhmedia.com>
2014-08-18 20:18:19 -0400

    Get tests to pass

--- test/syslog-test.js
+++ test/syslog-test.js
@@ -10,40 +10,28 @@
  */
 
 var path = require('path'),
-vows = require('vows'),
-assert = require('assert'),
-winston = require('winston'),
-helpers = require('winston/test/helpers'),
-SyslogNative = require('../lib/winston-syslog-native').SyslogNative;
+    vows = require('vows'),
+    assert = require('assert'),
+    winston = require('winston'),
+    helpers = require('winston/test/helpers'),
+    SyslogNative = require('../lib/winston-syslog-native').SyslogNative;
 
-function assertSyslog (transport) {
-  assert.instanceOf(transport, SyslogNative);
-  assert.isFunction(transport.log);
-  assert.isFunction(transport.connect);
+function assertSyslogNative (transport) {
+      assert.instanceOf(transport, SyslogNative);
+      assert.isFunction(transport.log);
 };
-
-function closeTopic(){
-  var transport = new winston.transports.SyslogNative();
-  // var logger = new winston.Logger({
-  //   transports: [transport]
-  // });
-  // logger.log('debug', 'Test message');
-  return transport;
-}
 
 var transport = new SyslogNative();
 
 vows.describe('winston-syslog-native').addBatch({
-  "An instance of the Syslog Transport": {
-    "should have the proper methods defined": function () {
-      console.error('running');
-      assertSyslog(transport);
-    },
-    "the log() method": helpers.testSyslogLevels(transport, "should log messages to syslogd", function (ign, err, ok) {
-      assert.isTrue(!err);
-      assert.isTrue(ok);
-    }),
-    topic : closeTopic
-  }
+    "An instance of the Rsyslog Transport": {
+        "should have the proper methods defined": function () {
+            assertSyslogNative(transport);
+        },
+        "the log() method": helpers.testSyslogLevels(transport, "should log messages to rsyslog", function (ign, err, logged) {
+            assert.isNull(err);
+            assert.isTrue(logged);
+        })
+    }
 }).export(module);
 
